fdc0d31d9b1bf3eacf1b49b045822d2e238ed485
Riha117 <snriha117@gmail.com>
2026-05-24 00:55:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
-
 import streamlit as st
 import pandas as pd
 import joblib
 
-# Load trained model
+# Load model
 model = joblib.load("diabetes_model.pkl")
 
 # Title
-st.title("Diabetes Risk Prediction App")
+st.title("Diabetes Risk Prediction")
 
-st.write("Enter health details below:")
+st.write("Enter patient details below:")
 
-# User Inputs
-pregnancies = st.number_input("Pregnancies", min_value=0, step=1)
+# Inputs
+pregnancies = st.number_input("Pregnancies", min_value=0)
 
-glucose = st.number_input("Glucose Level", min_value=0)
+glucose = st.number_input("Glucose", min_value=0)
 
 blood_pressure = st.number_input("Blood Pressure", min_value=0)
 
 skin_thickness = st.number_input("Skin Thickness", min_value=0)
 
-insulin = st.number_input("Insulin Level", min_value=0)
+insulin = st.number_input("Insulin", min_value=0)
 
 bmi = st.number_input("BMI", min_value=0.0)
 
@@ -28,25 +27,23 @@ dpf = st.number_input("Diabetes Pedigree Function", min_value=0.0)
 
 age = st.number_input("Age", min_value=1)
 
-# Predict Button
-if st.button("Predict Diabetes Risk"):
+# Predict
+if st.button("Predict"):
 
-   input_data = pd.DataFrame([{
-    "Pregnancies": pregnancies,
-    "Glucose": glucose,
-    "BloodPressure": blood_pressure,
-    "SkinThickness": skin_thickness,
-    "Insulin": insulin,
-    "BMI": bmi,
-    "DiabetesPedigreeFunction": dpf,
-    "Age": age
-}])
+    input_data = pd.DataFrame([{
+        "Pregnancies": pregnancies,
+        "Glucose": glucose,
+        "BloodPressure": blood_pressure,
+        "SkinThickness": skin_thickness,
+        "Insulin": insulin,
+        "BMI": bmi,
+        "DiabetesPedigreeFunction": dpf,
+        "Age": age
+    }])
 
     prediction = model.predict(input_data)[0]
 
     probability = model.predict_proba(input_data)[0][1]
-
-    st.subheader("Prediction Result")
 
     if prediction == 1:
         st.error(f"High Diabetes Risk ({probability:.2%})")
