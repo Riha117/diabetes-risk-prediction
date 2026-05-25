014fb970f429dc9f393f0985eed9cc2fc5ade8ee
Riha117 <snriha117@gmail.com>
2026-05-25 20:39:54 +0530

    Add files via upload

---
+++ app (2).py
@@ -0,0 +1,57 @@
+
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Load trained model
+model = joblib.load("diabetes_model.pkl")
+
+# Title
+st.title("Diabetes Risk Prediction App")
+
+st.write("Enter health details below:")
+
+# User Inputs
+col1, col2 = st.columns(2)
+
+with col1:
+    pregnancies = st.number_input("Pregnancies", min_value=0)
+    glucose = st.number_input("Glucose", min_value=0)
+    blood_pressure = st.number_input("Blood Pressure", min_value=0)
+    skin_thickness = st.number_input("Skin Thickness", min_value=0)
+
+with col2:
+    insulin = st.number_input("Insulin", min_value=0)
+    bmi = st.number_input("BMI", min_value=0.0)
+    dpf = st.number_input("Diabetes Pedigree Function", min_value=0.0)
+    age = st.number_input("Age", min_value=1)
+# Predict Button
+if st.button("Predict Diabetes Risk"):
+
+    input_data = pd.DataFrame([{
+        "Pregnancies": pregnancies,
+        "Glucose": glucose,
+        "BloodPressure": blood_pressure,
+        "SkinThickness": skin_thickness,
+        "Insulin": insulin,
+        "BMI": bmi,
+        "DiabetesPedigreeFunction": dpf,
+        "Age": age
+    }])
+
+    prediction = model.predict(input_data)[0]
+
+    probability = model.predict_proba(input_data)[0][1]
+
+    st.subheader("Prediction Result")
+  
+    st.subheader("Prediction Probability")
+
+    st.progress(float(probability))
+
+    st.write(f"Risk Probability: {probability:.2%}")
+
+    if prediction == 1:
+        st.error(f"High Diabetes Risk ({probability:.2%})")
+    else:
+        st.success(f"Low Diabetes Risk ({probability:.2%})")
